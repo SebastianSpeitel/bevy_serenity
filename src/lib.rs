5c9c01430c5fe918a98f7bdbc302631a4912a36d
Sebastian Speitel <sebastian.speitel@outlook.de>
2024-01-30 20:55:09 +0100

    fix: actually run the client

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::ops::Deref;
 use std::pin::Pin;
 use std::sync::mpsc::{channel, Receiver, Sender};
 use std::sync::{Arc, Mutex, OnceLock};
@@ -17,6 +18,11 @@ struct Starting(OnceLock<StartingFut>);
 unsafe impl Send for Starting {}
 unsafe impl Sync for Starting {}
 
+type RunningFut = Compat<Pin<Box<dyn std::future::Future<Output = ()> + Send + Sync>>>;
+
+#[derive(Component)]
+struct Running(OnceLock<RunningFut>);
+
 #[derive(Component, Clone, Debug)]
 pub struct SerenityConfig {
     pub token: String,
@@ -24,7 +30,15 @@ pub struct SerenityConfig {
 }
 
 #[derive(Component)]
-pub struct SerenityClient(serenity::Client);
+pub struct SerenityClient(Arc<serenity::http::Http>);
+
+impl Deref for SerenityClient {
+    type Target = serenity::http::Http;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
 type RawEvent = (serenity::all::Event, Context);
 
@@ -40,7 +54,10 @@ struct SenderHandler(Sender<RawEvent>);
 #[serenity::async_trait]
 impl RawEventHandler for SenderHandler {
     async fn raw_event(&self, ctx: Context, ev: serenity::all::Event) {
-        log::trace!("Received raw event: {:?}", ev);
+        log::trace!(
+            "Received raw event: {}",
+            ev.name().unwrap_or_else(|| "Unknown".to_owned())
+        );
         self.0.send((ev, ctx)).unwrap();
     }
 }
@@ -80,14 +97,14 @@ fn start(mut commands: Commands, clients: Query<(Entity, &SerenityConfig), Added
 }
 
 fn poll_starting(mut commands: Commands, mut clients: Query<(Entity, &mut Starting)>) {
-    for (client, mut starting) in &mut clients {
+    for (entity, mut starting) in &mut clients {
         let fut = starting.0.get_mut().unwrap();
 
         let Some(res) = future::block_on(future::poll_once(fut)) else {
             continue;
         };
-        commands.entity(client).remove::<Starting>();
-        let c = match res {
+        commands.entity(entity).remove::<Starting>();
+        let mut c = match res {
             Ok(c) => c,
             Err(e) => {
                 log::error!("Failed to start client: {:?}", e);
@@ -95,7 +112,15 @@ fn poll_starting(mut commands: Commands, mut clients: Query<(Entity, &mut Starti
             }
         };
         log::info!("Client started");
-        commands.entity(client).insert(SerenityClient(c));
+        let client = SerenityClient(c.http.clone());
+        let fut = async move {
+            let _ = c.start().await;
+        };
+        let fut: Pin<Box<dyn std::future::Future<Output = ()> + Send + Sync>> = Box::pin(fut);
+        let fut = Compat::new(fut);
+        let cell = OnceLock::new();
+        let _ = cell.set(fut);
+        commands.entity(entity).insert((Running(cell), client));
     }
 }
 
@@ -103,10 +128,9 @@ fn write_events(
     receivers: Query<(Entity, &RawEventReceiver)>,
     mut writer: EventWriter<SerenityEvent>,
 ) {
-    for (entity, receiver) in &mut receivers.iter() {
+    for (entity, receiver) in &receivers {
         let receiver = receiver.0.lock().unwrap();
         for (event, context) in receiver.try_iter() {
-            log::trace!("Received event: {:?}", event);
             let ev = SerenityEvent {
                 entity,
                 event,
@@ -117,11 +141,18 @@ fn write_events(
     }
 }
 
+fn run(mut clients: Query<&mut Running>) {
+    for mut running in &mut clients {
+        let fut = running.0.get_mut().unwrap();
+        let _ = future::block_on(future::poll_once(fut));
+    }
+}
+
 pub struct SerenityPlugin;
 
 impl bevy_app::Plugin for SerenityPlugin {
     fn build(&self, app: &mut bevy_app::App) {
         app.add_event::<SerenityEvent>();
-        app.add_systems(bevy_app::Update, (start, poll_starting, write_events));
+        app.add_systems(bevy_app::Update, (start, poll_starting, write_events, run));
     }
 }
